78df64ab7cda526a2bc5b2173d9cc73b3ccd5231
vital <vitaliykastsiuk@gmail.com>
2015-04-13 14:48:53 +0300

    change getting full host

--- lib/omniauth/strategy.rb
+++ lib/omniauth/strategy.rb
@@ -402,21 +402,21 @@ module OmniAuth
     end
 
     def full_host
-      case OmniAuth.config.full_host
-        when String
-          OmniAuth.config.full_host
-        when Proc
-          OmniAuth.config.full_host.call(env)
-        else
-          if self.class ==  OmniAuth::Strategies::Vkontakte && (Rails.env.production? || Rails.env.staging?)
-            AppConfig.vk_redirect_uri
+      if self.class ==  OmniAuth::Strategies::Vkontakte && (Rails.env.production? || Rails.env.staging?)
+        AppConfig.vk_redirect_uri
+      else
+        case OmniAuth.config.full_host
+          when String
+            OmniAuth.config.full_host
+          when Proc
+            OmniAuth.config.full_host.call(env)
           else
             uri = URI.parse(request.url.gsub(/\?.*$/,''))
             uri.path = ''
             #sometimes the url is actually showing http inside rails because the other layers (like nginx) have handled the ssl termination.
             uri.scheme = 'https' if ssl?
             uri.to_s
-          end
+        end
       end
     end
 
